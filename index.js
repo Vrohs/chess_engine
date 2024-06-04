0f4f1285be02057a7002de5de334171c3ea1bbc3
Vrohs <info.rohsgaming536@gmail.com>
2024-06-04 11:23:35 +0530

    chore: update npm dependencies for chess game UI

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const socket = require('socket.io');
+const http = require('http');
+const {Chess} = require('chess.js');
+const path = require('path');
+const PORT = 5600;
+
+const server = http.createServer(app);
+const io = socket(server);
+
+const chess = new Chess();
+const players = {};
+let currPlayer = 'W';
+
+app.set('view engine', 'ejs');
+app.use(express.static('.'));
+
+
+app.get('/', (req,res)=>{
+
+    res.render('playground')
+})
+
+server.listen(PORT, (err)=>{
+
+    if(err){
+        console.error(err);
+    } else {
+    console.log(`server active on http://localhost:${PORT}`)
+}})
